86f4daf7a6021b383532a700e76c37e4d8b364a4
jht <497027703@qq.com>
2018-11-02 21:18:20 +0800

    show

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    猫眼电影
+    猫眼电影 <mt-button>mint-ui组件库提供的组件</mt-button>
   </div>
 </template>
+<script>
+import { Button } from "mint-ui";
+export default {
+  components:{
+    [Button.name]:Button
+  }
+}
+</script>
+
 <style lang="stylus" scoped>
 @import '~styles/variables.styl'
 div
